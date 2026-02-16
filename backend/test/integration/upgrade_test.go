3d248af659057cb2dad2496b6be9d62a666da362
Alyssa Goins <agoins@redhat.com>
2026-02-16 10:48:41 -0500

    Replace docker push with kind load docker-image.
    Signed-off-by: Nelesh Singla <117123879+nsingla@users.noreply.github.com>

--- backend/test/integration/upgrade_test.go
+++ backend/test/integration/upgrade_test.go
@@ -255,6 +255,8 @@ func (s *UpgradeTests) VerifyExperiments() {
 	assert.NotEmpty(t, experiments[4].ID)
 	assert.NotEmpty(t, experiments[4].CreatedAt)
 
+	// Note: "hello world experiment" is created later in VerifyCreatingRunsAndJobs,
+	// so it is not present at this point.
 }
 
 // TODO(jingzhang36): prepare pipeline versions.
@@ -272,7 +274,7 @@ func (s *UpgradeTests) PreparePipelines() {
 	time.Sleep(1 * time.Second)
 	sequentialPipeline, err := s.pipelineClient.Create(&pipelineParams.PipelineServiceCreatePipelineV1Params{
 		Body: &pipeline_model.APIPipeline{Name: "sequential", URL: &pipeline_model.APIURL{
-			PipelineURL: "https://raw.githubusercontent.com/opendatahub-io/data-science-pipelines/refs/heads/master/backend/test/v2/resources/sequential.yaml",
+			PipelineURL: "https://raw.githubusercontent.com/opendatahub-io/data-science-pipelines/refs/tags/v2.18.0/backend/test/v2/resources/sequential.yaml",
 		}},
 	})
 	require.Nil(t, err)
@@ -286,7 +288,7 @@ func (s *UpgradeTests) PreparePipelines() {
 	assert.Equal(t, "zip-arguments-parameters", argumentUploadPipeline.Name)
 
 	/* ---------- Import pipeline tarball by URL ---------- */
-	pipelineURL := "https://github.com/opendatahub-io/data-science-pipelines/raw/refs/heads/master/backend/test/v2/resources/arguments.pipeline.zip"
+	pipelineURL := "https://github.com/opendatahub-io/data-science-pipelines/raw/refs/tags/v2.18.0/backend/test/v2/resources/arguments.pipeline.zip"
 
 	if pullNumber := os.Getenv("PULL_NUMBER"); pullNumber != "" {
 		pipelineURL = fmt.Sprintf("https://raw.githubusercontent.com/red-hat-data-services/data-science-pipelines/pull/%s/head/backend/test/v2/resources/arguments.pipeline.zip", pullNumber)
